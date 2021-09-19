02a7c8b061837a94460084e2db26ac99de30f741
htsuyoshi <henriquetsuyoshiy@gmail.com>
2021-09-19 02:10:13 -0300

    Add buffers, clear and start threshold

--- img2ascii.cpp
+++ img2ascii.cpp
@@ -1,41 +1,94 @@
 #include <Magick++.h> 
+#include <filesystem>
 #include <iostream> 
-#include <stdio.h>
+#include <fstream> 
 #include <cstring>
-
-#include <typeinfo>
 
 using namespace std; 
 using namespace Magick; 
+using namespace filesystem; 
 
 #include "braille_data.h"
 #include "strings.h"
 #include "argh.h"
 
-int main(int argc,char **argv) 
-{ 
-    Image image;
+#define waifu_directory "/tmp/waifu/"
+#define waifu_file(a) "/tmp/waifu/" + to_string(a)
+
+/* File manipulation */
+
+void print_buff(int file_index) {
+    ifstream file;
+    file.open(waifu_file(file_index), ios::in);
+
+    if (file.is_open()) {
+        string line;
+        while(getline(file, line))
+            cout << line << endl;
+    }
+    file.close();
+}
+
+int main(int argc,char **argv) { 
     try { 
         argh::parser cmdl(argc, argv);
-
-        /* No arguments */
-        if (!cmdl(1)) {
-            cout << arguments << endl;
-            cout << usage << endl;
-            return 1;
-        }
+        Image image;
+        ColorGray cor;
 
         bool first_arg = true;
-        double average = 0;
+        double threshold = 0;
         int max_columns, 
             max_rows,
             columns,
             rows,
-            scale;
+            scale,
+            file_index;
 
-        ColorGray cor;
+        /* --- Set environment --- */
 
-        /* Iterate over images */
+        /* Create folder to ascii art*/
+        path folder{waifu_directory};
+        if (!exists(folder))
+            create_directory(folder);
+
+        /* --- Errors --- */
+
+        /* No arguments */
+        if (!cmdl(1) && !cmdl({"-b","--buffer"})) {
+            cout << arguments;
+            cout << usage;
+            return 1;
+        }
+
+
+        /* --- Options --- */
+
+        /* Print from buffer */
+        if (cmdl({"-b","--buffer"})) {
+            cmdl({"-b","--buffer"}) >> file_index;
+            if (exists(waifu_file(file_index))) {
+                print_buff(file_index);
+                return 0;
+            }
+        }
+
+        /* Clear buffer */
+        if (cmdl({"-c","--clear"})) {
+            for (auto const& file: directory_iterator{folder})
+                remove(file);
+            cout << cleared;
+            return 0;
+        }
+
+        /* Set threshold */
+        if (cmdl({"-t","--threshold"}))
+            cmdl({"-t","--threshold"}) >> threshold;
+
+        /* Scale image */
+        cmdl({"-s", "--scale"}, 200) >> scale;
+
+        /* --- Iterate over args --- */
+
         for (auto& images : cmdl) {
 
             if (first_arg) {
@@ -46,49 +99,69 @@ int main(int argc,char **argv)
             /* Read a file into image object */
             image.read( images );
 
-            /* Scale image */
-            cmdl({"-s", "--scale"}, 100) >> scale;
             image.scale(Geometry(scale, scale));
+
             max_columns = image.columns();
             max_rows = image.rows();
 
             /* Get average */
-            for(int i=0; i<max_columns; i++) {
-                for(int j=0; j<max_rows; j++) {
-                    cor = image.pixelColor(i,j);
-                    average += cor.shade();
-            }}
-            average = average / (max_rows * max_columns);
+            if (!cmdl({"-t","--threshold"})) {
+                for (int i=0; i<max_columns; i++) {
+                    for (int j=0; j<max_rows; j++) {
+                        cor = image.pixelColor(i,j);
+                        threshold += cor.shade();
+                }}
+                threshold = threshold / (max_rows * max_columns);
+            }
 
-            /* Create ascii art */
             columns = (int)((max_columns/2)+1);
             rows = (int)((max_rows/4)+1);
+
+            /* Create ascii art */
             string ascii_art[rows][columns];
-            for(int i=0; i<max_columns; i+=2) {
-                for(int j=0; j<max_rows; j+=4) {
+            for (int i=0; i<max_columns; i+=2) {
+                for (int j=0; j<max_rows; j+=4) {
 
                     int character = 0;
-                    for(int r=0; r<4; r++) {
-                        for(int c=0; c<2; c++) {
+                    for (int r=0; r<4; r++) {
+                        for (int c=0; c<2; c++) {
                             cor = image.pixelColor(i + c,j + r);
-                            if(cor.shade() > average) {
+                            if (cor.shade() > threshold)
                                 character += 1 << (r*2 + c);
-                            }
                     }}
 
                     ascii_art[(int)(j/4)][(int)(i/2)] = (string) braille_data[character];
             }}
 
-            for(int i=0; i<rows; i++) {
-                for(int j=0; j<columns; j++) {
+            /* Print the ascii art */
+            for (int i=0; i<rows; i++) {
+                for (int j=0; j<columns; j++) {
                     cout << ascii_art[i][j];
                 }
                 cout << endl;
             }
-        }
-    } 
-    catch( Exception &error_ ) 
-    { 
+
+            /* Save in a buffer */
+            if (cmdl({"-b","--buffer"})) {
+                cmdl({"-b", "--buffer"}, 1) >> file_index;
+
+                if (exists(waifu_file(file_index))) {
+                    cout << buff_used;
+                    return 1;
+                }
+
+                ofstream file;
+                file.open(waifu_file(file_index), ios::out);
+
+                for (int i=0; i<rows; i++) {
+                    for (int j=0; j<columns; j++) {
+                        file << ascii_art[i][j];
+                    }
+                    file << endl;
+                }
+                file.close();
+        }}
+    } catch ( Exception &error_ ) { 
         cout << "Caught exception: " << error_.what() << endl; 
         return 1; 
     } 
--- strings.h
+++ strings.h
@@ -1,3 +1,5 @@
-string usage = "Usage: img2ascii [OPTIONS] <file> ...";
-string arguments = "Must provide at least 1 file";
-string example_size = "Usage: img2ascii -s=100";
+string usage = "Usage: img2ascii [OPTIONS] <file> ...\n";
+string arguments = "Must provide at least 1 file\n";
+string example_size = "Usage: img2ascii -s=100\n";
+string cleared = "Buffer is clear\n";
+string buff_used = "This Buffer is already in use\n";
